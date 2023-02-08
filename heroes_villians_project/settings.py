c7b95cd936c727e622067cf0b8c67c810f11901f
Amy Kaminski <amykaminski928@gmail.com>
2023-02-08 16:23:50 -0600

    error persists for get by id: type not iterable

--- heroes_villians_project/settings.py
+++ heroes_villians_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'super_types',
-    'supers',    
+    'supers'  
 ]
 
 MIDDLEWARE = [
--- supers/views.py
+++ supers/views.py
@@ -3,33 +3,39 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .seializers import SuperSerializer
 from .models import Super
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
 @api_view (['GET'])
 def supers_list (request):
     supers = Super.objects.all()
-    supers_serializer = SuperSerializer(supers, many = True)
-    
-    heroes = supers.filter(super_types__type='hero')
-    villains = supers.filter(super_types__type='villain')
+    serializer = SuperSerializer(supers, many = True)
+    heroes = supers.filter(super_types__type='1')
+    villains = supers.filter(super_types__type='2')
     serialized_heroes = SuperSerializer(heroes, many = True)
     serialized_villains = SuperSerializer(villains, many = True)
-
-  
-       
+           
     custom_response_dict = {
         'Heroes': [serialized_heroes.data],
         'Villians': [serialized_villains.data]
     }
-        
-
     return Response(custom_response_dict)
    
-
+@api_view (['GET', 'PUT'])
+def supers_detail(request, pk):
+    supers = Super.objects.get(pk=pk)
     
+    if request.method == 'GET':
+        try:
+            serializer = SuperSerializer(super, many=True)
+            return Response(serializer.data)
+        except Super.DoesNotExist:
+            return Response(status = status.HTTP_404_NOT_FOUND)
     
-        
-
-        
- 
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
