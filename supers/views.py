6db1a455bcf2576d61826a914afd60945790e989
Amy Kaminski <amykaminski928@gmail.com>
2023-02-08 08:59:49 -0600

    Working on query requests and paths

--- supers/views.py
+++ supers/views.py
@@ -1,9 +1,16 @@
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .seializers import SuperSerializer
+from .models import Super
 
 # Create your views here.
 @api_view (['GET'])
 def supers_list (request):
+    supers = Super.objects.all()
+    serializer = SuperSerializer(supers, many = True)
 
-    return Response('ok')
+    if request.method == 'GET':
+        super_type = request.query_params.get('super_type')
+        print(super_type)    
+    return Response(serializer.data)
