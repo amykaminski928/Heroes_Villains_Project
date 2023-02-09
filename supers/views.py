88a1e76e848ceda4b701b2dd10c500d0eba24ce4
Amy Kaminski <amykaminski928@gmail.com>
2023-02-08 23:20:09 -0600

    added PUT, GET by ID and POST requests + working to correct code so that all requests run.

--- supers/views.py
+++ supers/views.py
@@ -8,21 +8,30 @@ from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
-@api_view (['GET'])
+@api_view (['GET', 'POST'])
 def supers_list (request):
     supers = Super.objects.all()
     serializer = SuperSerializer(supers, many = True)
-    heroes = supers.filter(super_types__type='1')
-    villains = supers.filter(super_types__type='2')
+    heroes = supers.filter(super_type__type= 'hero')
+    villains = supers.filter(super_type__type= 'villain')
     serialized_heroes = SuperSerializer(heroes, many = True)
     serialized_villains = SuperSerializer(villains, many = True)
-           
-    custom_response_dict = {
+    if request.method == 'GET':       
+        custom_response_dict = {
         'Heroes': [serialized_heroes.data],
         'Villians': [serialized_villains.data]
-    }
-    return Response(custom_response_dict)
+        }
+        return Response(custom_response_dict)
+        
+    if request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data)
+        
+    
    
+
 @api_view (['GET', 'PUT'])
 def supers_detail(request, pk):
     supers = Super.objects.get(pk=pk)
